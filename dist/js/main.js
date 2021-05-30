6b38654809904fc74c48c1fa5e0948294061d6d7
Walny Valdez <walnyvaldez@gmail.com>
2021-05-30 11:46:24 -0400

    6th commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -287,16 +287,4 @@
         }
     });
 
-    /**
-     * Animation on scroll
-     */
-    window.addEventListener('load', () => {
-        AOS.init({
-            duration: 1000,
-            easing: "ease-in-out",
-            once: true,
-            mirror: false
-        });
-    });
-
 })()
